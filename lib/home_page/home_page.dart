3885719332842ee37f83db695e6a6bceff933d4d
ForsythIshan <ishand1005@gmail.com>
2024-04-06 22:57:29 +0530

    feat: made the images clickable

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -34,44 +34,77 @@ class HomePageState extends State<HomePage> {
         shrinkWrap: true,
         itemCount: titles.length,
         itemBuilder: (context, index) {
-          return Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Column(
-              children: [
-                Expanded(
-                  child: ClipRRect(
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.elliptical(20, 15),
-                      topRight: Radius.elliptical(20, 15),
-                      bottomLeft: Radius.elliptical(20, 15),
-                      bottomRight: Radius.elliptical(20, 15),
-                    ),
-                    child: Image.network(
-                      'https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p7825626_p_v8_af.jpg',
-                      fit: BoxFit
-                          .cover, // Ensure the image covers the entire area
-                      loadingBuilder: (context, child, loadingProgress) =>
-                          loadingProgress == null
-                              ? child
-                              : const Center(
-                                  child: CircularProgressIndicator(),
-                                ),
+          return GestureDetector(
+            onTap: () {
+              // Navigate to movie details page
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      MovieDetailsPage(movieTitle: titles[index]),
+                ),
+              );
+            },
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.elliptical(10, 5),
+                  bottomRight: Radius.elliptical(10, 5),
+                  topRight: Radius.elliptical(10, 5),
+                  bottomLeft: Radius.elliptical(10, 5),
+                ),
+              ),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.elliptical(10, 8.5),
+                        bottomRight: Radius.elliptical(10, 8.5),
+                        topRight: Radius.elliptical(10, 8.5),
+                        bottomLeft: Radius.elliptical(10, 8.5),
+                      ),
+                      child: Image.network(
+                        'https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p7825626_p_v8_af.jpg',
+                        fit: BoxFit.cover,
+                        loadingBuilder: (context, child, loadingProgress) =>
+                            loadingProgress == null
+                                ? child
+                                : const Center(
+                                    child: CircularProgressIndicator(),
+                                  ),
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 2.5),
-                Text(
-                  titles[index],
-                  style: const TextStyle(
-                    fontSize: 12,
+                  const SizedBox(height: 2.5),
+                  Text(
+                    titles[index],
+                    style: const TextStyle(
+                      fontSize: 12,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+class MovieDetailsPage extends StatelessWidget {
+  final String movieTitle;
+  const MovieDetailsPage({super.key, required this.movieTitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(movieTitle),
+      ),
+      body: Center(
+        child: Text('Details of $movieTitle'),
       ),
     );
   }
